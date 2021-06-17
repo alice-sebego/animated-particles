6de4cf4c65d8a10f0a7a8c610ba6dbc0ed26422e
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-06-17 23:02:09 +0200

    Start to build context for canvas

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import particle from "./particle";
+
+const $canvas1 = document.querySelector("#canvas1");
+const ctx = canvas.getContext("2d");
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerheight;
---
+++ src/js/particle.js
@@ -0,0 +1,12 @@
+export default class particle{
+
+    constructor(x, y, directionX, directionY, size, color){
+        this.x = x;
+        this.y = y;
+        this.directionX = directionX;
+        this.directionY = directionY;
+        this.size = size;
+        this.color = color;
+    }
+    
+}
