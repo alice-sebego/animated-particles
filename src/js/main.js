976b4386c49a0f2d25d6b5270fa6c06be731e404
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-06-18 21:48:58 +0200

    Create 100 particles

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,22 @@
 import Particle from "./particle.js";
 
-const circle1 = new Particle(100, 100, 50, 50, 100, "red");
+let particlesTab = [];
+
+const circle1 = new Particle(300, 300, 50, 50, 100, "red");
 circle1.draw();
+
+const init = () => {
+    for(let i = 0; i < 100; i++){
+        let size = Math.random() + 0.01 * 20;
+        let x = Math.random() * (window.innerWidth - size * 2);
+        let y = Math.random() * (window.innerHeight - size * 2);
+        let directionX = (Math.random() * 0.4) - 0.2;
+        let directionY = (Math.random() * 0.4) - 0.2;
+        let color = "white";
+
+        particlesTab.push(new Particle(x, y, directionX, directionY, size, color));
+    }
+}
+
+init();
+console.log(particlesTab)
